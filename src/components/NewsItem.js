e78f1476a2c0ad3010fff1807672d35bc3d06322
Aditya Nigam <nigamadi2209@gmail.com>
2022-04-05 01:19:06 +0530

    refactoring done  excluding app.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+const NewsItem =(props) =>{
+
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
     return (
       <div className="my-3">
         <div className="card">
@@ -28,7 +28,7 @@ export class NewsItem extends Component {
         </div>
       </div>
     );
-  }
+  
 }
 
 export default NewsItem;
